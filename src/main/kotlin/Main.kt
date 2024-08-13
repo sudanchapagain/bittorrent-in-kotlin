e57e1bdc84820f6588f2fff128802a7665c18d8e
Your Name <you@example.com>
2024-08-13 21:53:07 +0545

    codecrafters submit [skip ci]

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,16 @@
 import com.google.gson.Gson;
-// import com.dampcake.bencode.Bencode; - available if you need it!
+import com.dampcake.bencode.Bencode;
 
 val gson = Gson()
 
 fun main(args: Array<String>) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println("Logs from your program will appear here!")
     val command = args[0]
     when (command) {
         "decode" -> {
-            // Uncomment this block to pass the first stage
-            // val bencodedValue = args[1]
-            // val decoded = decodeBencode(bencodedValue)
-            // println(gson.toJson(decoded))
-            // return
+             val bencodedValue = args[1]
+             val decoded = decodeBencode(bencodedValue)
+             println(gson.toJson(decoded))
+             return
         }
         else -> println("Unknown command $command")
     }
